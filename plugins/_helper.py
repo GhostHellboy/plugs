384bef88c0bcb43abd8d58281660515047f87e47
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-20 15:30:20 +0530

    Update _helper.py

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -1,15 +1,18 @@
+from userbot.uniborgConfig import Config
 import asyncio
 
 import requests
-from userbot import SUDO_LIST
-from userbot.uniborgConfig import Config
+from telethon import functions
+
+from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@bot.on(admin_cmd(pattern="help", outgoing=True))
+@bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
 async def yardim(event):
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
-    if tgbotusername is not None:
+    input_str = event.pattern_match.group(1)
+    if tgbotusername is not None or hell_input == "text":
         results = await event.client.inline_query(tgbotusername, "@HellBot_Official")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
@@ -17,6 +20,15 @@ async def yardim(event):
         await event.delete()
     else:
         await edit_or_reply(event, ["NO_BOT"])
+   elif input_str:
+       if input_str in CMD_LIST:
+           string = "Commands found in {}:\n".format(input_str)
+           for i in CMD_LIST[input_str]:
+               string += "  " + i
+               string += "\n"
+           await event.edit(string)
+       else:
+           await event.edit(input_str + " is not a valid plugin!")
 
 
 @bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
